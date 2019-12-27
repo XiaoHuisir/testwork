79602db7850d37faa1b4fd16ed1ece7ffc7e4d5a
XiaoHuisir <860663594@qq.com>
2019-12-27 21:34:00 +0800

    修改了视频全屏时横屏切换问题，和手机9.0登陆问题，和一些布局的修改

--- app/src/main/java/com/example/myapplication/ui/fragment/CourseFragment.java
+++ app/src/main/java/com/example/myapplication/ui/fragment/CourseFragment.java
@@ -1,12 +1,15 @@
 package com.example.myapplication.ui.fragment;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.myapplication.R;
 import com.example.myapplication.adaper.IndexAdapter;
 import com.example.myapplication.app.Constant;
@@ -17,6 +20,10 @@ import com.example.myapplication.interfaces.contract.IndexConstract;
 import com.example.myapplication.presenter.home.IndexPresenter;
 import com.example.myapplication.ui.acivity.video.VideoActivity;
 import com.example.myapplication.widgets.DrawableCenterTextView;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+import com.youth.banner.loader.ImageLoader;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -38,9 +45,11 @@ public class CourseFragment extends BaseFragment implements IndexConstract.View,
     DrawableCenterTextView txtType2;
     @BindView(R.id.txt_type_3)
     DrawableCenterTextView txtType3;
-
+    @BindView(R.id.banner)
+    Banner banner;
     IndexAdapter indexAdapter;
     List<IndexBean.DataBean.CurriculumDataBean> list;
+
     @Override
     protected IBasePresenter getPresenter() {
         return new IndexPresenter();
@@ -102,21 +111,43 @@ public class CourseFragment extends BaseFragment implements IndexConstract.View,
     public void getIndexReturn(IndexBean result) {
         list.clear();
         list.addAll(result.getData().getCurriculum_data());
+        banners(result);
         indexAdapter.notifyDataSetChanged();
+
     }
 
+    private void banners(IndexBean result) {
+        ArrayList<String> strings = new ArrayList<>();
+        List<IndexBean.DataBean.LbDataBean> lb_data = result.getData().getLb_data();
+        for (int i = 0; i < lb_data.size(); i++) {
+            String image = lb_data.get(i).getImage();
+            strings.add(image);
+        }
+        banner.setImages(strings)
+        .setImageLoader(new MyLoader())
+        .setDelayTime(2000)
+        .isAutoPlay(true)
+        .setIndicatorGravity(BannerConfig.CENTER)
+        .setBannerAnimation(Transformer.Accordion).start();
+    }
+    class MyLoader extends ImageLoader {
+        @Override
+        public void displayImage(Context context, Object path, ImageView imageView) {
+            String loads = (String) path;
+            Glide.with(context).load(loads).into(imageView);
+        }
+    }
 
     private void resetTypeTxt() {
         txtType1.setTextColor(getResources().getColor(R.color.black));
         txtType2.setTextColor(getResources().getColor(R.color.black));
         txtType3.setTextColor(getResources().getColor(R.color.black));
     }
-
     @Override
     public void click(String id) {
         Intent intent = new Intent();
         intent.setClass(context, VideoActivity.class);
-        intent.putExtra("curriulum_id",id);
+        intent.putExtra("curriulum_id", id);
         startActivity(intent);
     }
 }
